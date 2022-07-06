db9e6d3a9ada3b08f9b0bda2632659845c7d0b6b
danielstewart914 <103592919+danielstewart914@users.noreply.github.com>
2022-07-06 16:12:37 -0700

    Started on weather display functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,90 @@
 var openWeatherApiRootUrl = 'http://api.openweathermap.org';
 var openWeatherApiKey = '1008cf8d4beec486e3a918845ef87da6';
-var units = 'imperial';
+var getWeatherButtonEl = $( '#getWeather' );
+var toggleUnitsEl = $( '#toggleUnits' );
+var citySearchFormEl = $( '#citySearch' );
+var cityInputEl = $( '#cityInput' );
+var mainEl = $( '#main' );
+var currentWeatherDisplayEl = $( '#currentWeatherDisplay' );
+
+const metric = { 
+    query: 'metric',
+    speed: 'm/s',
+    tempChar: '&#8451;'
+ };
+
+ const imperial = { 
+    query: 'imperial',
+    speed: 'mph',
+    tempChar: '&#8457;'
+ };
+
+ var units = imperial;
+
+function toggleUnits ( ) {
+
+    if ( units.query === 'imperial' ) {
+
+        units = metric;
+
+    } else {
+
+        units = imperial;
+
+    }
+
+    toggleUnitsEl.html( units.tempChar );
+
+}
+
+function degToCardinal ( degrees ) {
+
+    if ( degrees < 22.5 ) return 'N';
+    if ( degrees < 67.5 ) return 'NE';
+    if ( degrees < 112.5 ) return 'E';
+    if ( degrees < 157.5 ) return 'SE';
+    if ( degrees < 202.5 ) return 'S';
+    if ( degrees < 247.5 ) return 'SW';
+    if ( degrees < 292.5 ) return 'W';
+    if ( degrees < 337.5 ) return 'NW';
+    return 'N';
+}
 
 function displayCurrentWeather ( cityName, currentWeatherData ) {
+
+    var currentWeatherFrag = $( document.createDocumentFragment() );
+
+    var cityNameEl = $( '<h2>' );
+    var weatherDescEl = $( '<p>' );
+    var tempEl = $( '<p>' );
+    var windSpeedEl = $( '<p>' );
+    var humidityEl = $( '<p>' );
+    var uviEl = $( '<p>' );
+    var compassEl = $( '<div>' );
+
+
+    cityNameEl.text( cityName );
+    weatherDescEl.text( currentWeatherData.weather[0].main );
+    tempEl.html( 'Temp: ' + currentWeatherData.temp + ' ' + units.tempChar);
+
+    compassEl.addClass( 'compass' );
+    compassEl.html( '&#8593;' );
+    compassEl.css( 'transform', 'rotate( ' + currentWeatherData.wind_deg + 'deg )' );
+
+    windSpeedEl.append( `Wind: ${ currentWeatherData.wind_speed } ${ units.speed }`);
+    windSpeedEl.append( compassEl );
+    windSpeedEl.append( degToCardinal( currentWeatherData.wind_deg ) );
+    humidityEl.text( 'Humidity: ' + currentWeatherData.humidity + '%' );
+    uviEl.text( 'UV: ' + currentWeatherData.uvi );
+
+    currentWeatherFrag.append( cityNameEl );
+    currentWeatherFrag.append( weatherDescEl );
+    currentWeatherFrag.append( tempEl );
+    currentWeatherFrag.append( windSpeedEl );
+    currentWeatherFrag.append( humidityEl );
+    currentWeatherFrag.append( uviEl );
+
+    currentWeatherDisplayEl.html( currentWeatherFrag );
 
     console.log( cityName );
     console.log( currentWeatherData );
@@ -71,7 +153,7 @@ function getWeather ( location ) {
     var lat = location.lat;
     var lon = location.lon;
     var cityName = location.name;
-    var weatherUrl = `${ openWeatherApiRootUrl }/data/2.5/onecall?lat=${ lat }&lon=${ lon }&units=${ units }&exclude=minutely,hourly&appid=${ openWeatherApiKey }`;
+    var weatherUrl = `${ openWeatherApiRootUrl }/data/2.5/onecall?lat=${ lat }&lon=${ lon }&units=${ units.query }&exclude=minutely,hourly&appid=${ openWeatherApiKey }`;
 
     fetch ( weatherUrl )
 
@@ -102,4 +184,12 @@ function getWeather ( location ) {
 
 }
 
-getLatLong( 'Everett' );
+citySearchFormEl.submit( function ( event ) {
+
+    event.preventDefault();
+    getLatLong( cityInputEl.val() ); 
+    
+} );
+
+
+toggleUnitsEl.on( 'click', toggleUnits );
